4bb69b5075e0ee4b72ff5479823e2dd79e53a8ff
mysnik-htmlacademy <mysnik@htmlacademy.ru>
2021-06-07 01:18:30 +0300

    finished randomGet

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-const randomGet = function(low, high){
-  // TODO implement
+const randomGet = function(min, max) {
+  // NOTE implementation from https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  // NOTE in case of invalid parameters we return null
+  if ((min < 0) || (max <= min)) {
+    return null;
+  }
+
+  // NOTE include min and max values
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 const stringCheckLen = function(checkString, maxLen){
