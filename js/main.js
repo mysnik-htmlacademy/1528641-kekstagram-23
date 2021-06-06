b0a8ab85bb6e1b87c07bae5bad7492ab964638ac
mysnik-htmlacademy <mysnik@htmlacademy.ru>
2021-06-07 00:58:58 +0300

    boilerplate functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-const getSomethin = function() {
-// TODO implement
+const randomGet = function(low, high){
+  // TODO implement
 };
 
-getSomethin();
+const stringCheckLen = function(checkString, maxLen){
+  // TODO implement
+};
+
+// NOTE this invokations using for pass linter check
+randomGet(5, 8);
+stringCheckLen('Hello Kate', 20);
